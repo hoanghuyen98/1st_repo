c5c22cdecb844c97f279ab24a8a60b1051342d18
KQspck <huyen.techkids@gmail.com>
2019-08-01 21:26:32 +0700

    1st commit

---
+++ helloworld.py
@@ -0,0 +1,38 @@
+# print('hi dat! 123')
+
+# name = input('enter your name!')
+# print('hi',name)
+
+# hobby = input('enter your hobby: ')
+
+# print("your hobby is",hobby)
+
+# print(10 + 1)
+# print(10 - 1)
+# print(10 * 1)
+# print(10 / 1)
+
+# print(2**4)
+# print(5%2)
+# print(6//2)
+
+
+# print(type(10))
+
+# print(type("10"))
+# print(type('10'))
+
+# print('1'+'0')
+# print(1+0)
+
+# data = input('enter something: ')
+# print(type(data))
+
+
+
+#input nam sinh
+#print tuoi
+
+# year = int(input('enter your yob: '))
+# # print(year)
+# print(2019 - year)
